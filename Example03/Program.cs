9e4bceee7ab6fccf6294b794a1d377b48bd9e2b5
Andrei Shvetsov <shved963@gmail.com>
2022-12-06 19:57:03 +0300

    Example03 ready

---
+++ Example03/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+
+Console.WriteLine("Введите длину массива");
+int length = IntoInt();
+
+if (length < 0)
+{
+    Console.WriteLine("Incorrect number");
+    return;
+}
+
+Console.WriteLine("Введите элементы массива по порядку");
+
+int[] arr = GetArray(length);
+PrintArray(arr);
+
+
+void PrintArray(int[] array)
+{
+    string message = string.Join(" ,", array);
+    Console.WriteLine($"[{message}], ");
+}
+
+
+
+int[] GetArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        int element = IntoInt();
+        array[i] = element;
+    }
+    return array;
+}
+
+
+int IntoInt()
+{
+    bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+    if (!isParsed || num < 1)
+    {
+        return -1;
+    }
+    else
+    {
+        return num;
+    }
+}
