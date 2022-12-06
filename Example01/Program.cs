a3f9f1d2b6d64c43b4a3d33835d4fe3534b52dff
Andrei Shvetsov <shved963@gmail.com>
2022-12-06 18:58:45 +0300

    Example01 ready

---
+++ Example01/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая принимает на вход два числа (A и B) и
+// метод который возводит число A в натуральную степень B.
+// Задание должно быть выполнено в методе.
+// В методе не должно быть вывода в консоль. Не использовать Math.Pow()
+
+Console.Clear();
+
+int numberA = IntoInt();
+int numberB = IntoInt();
+
+double exponentOfNum = GetExponentOfNumber(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} = {exponentOfNum}");
+
+double GetExponentOfNumber(int numA, int numB)
+{
+    double numAb = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        numAb = numA * numAb;
+    }
+    return numAb;
+}
+
+int IntoInt()
+{
+    bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+    if (!isParsed || num < 1)
+    {
+        Console.WriteLine("Incorrect number");
+        return -1;
+    }
+    else
+    {
+        return num;
+    }
+}
+
+
+void Test()
+{
+    int[] introduced = new int[] { 1, 2, 3, 4 };
+    int exponent = 3;
+    int[] expected = new int[] { 1, 8, 27, 64 };
+    for (int i = 0; i < introduced.Length; i++)
+    {
+        double actual = GetExponentOfNumber(introduced[i], exponent);
+    
+        if (actual != expected[i])
+        {
+            Console.WriteLine($"Ожидали получить {expected[i]} а получили {actual}");
+        }
+        else
+        {
+            Console.WriteLine("okay");
+        }
+    }
+}
+
+//Test();
